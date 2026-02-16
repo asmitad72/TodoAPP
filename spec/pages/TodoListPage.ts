d94030ac6a5c814be122f576cb989944fd99d040
Asmita <asmitad72@gmail.com>
2026-02-17 00:03:04 +0530

    Lesson20

---
+++ spec/pages/TodoListPage.ts
@@ -0,0 +1,35 @@
+import {Locator, Page} from "@playwright/test";
+export class TodoListPage {
+    readonly page: Page;
+    readonly taskInputField: Locator;
+    readonly clearCompleted: Locator;
+    readonly taskItem: Locator;
+    readonly activeItemsFilter: Locator;
+    readonly completedItemsFilter: Locator;
+    readonly removeButton: Locator;
+    readonly completeTaskButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.taskInputField = page.getByTestId('text-input');
+        this.clearCompleted = page.locator('.clear-completed');
+        this.taskItem = page.getByTestId('todo-item-label');
+        this.activeItemsFilter = page.locator('[href="#/active"]');
+        this.completedItemsFilter = page.locator('[href="#/completed"]');
+        this.removeButton = page.getByRole('button', { name: '×' })
+        this.completeTaskButton= page.getByTestId('todo-item-toggle')
+    }
+
+    async addItem(itemName: string): Promise<void> {
+        await this.taskInputField.fill(itemName);
+        await this.taskInputField.press('Enter');
+    }
+    async removeTask(taskName:string) {
+        const taskItem = this.taskItem.filter({hasText: taskName});
+        await taskItem.hover();
+        await this.removeButton.click();
+    }
+    async completeTask(taskIndex:number) {
+        await this.completeTaskButton.nth(taskIndex).click();
+    }
+}
---
+++ spec/todo.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect } from '@playwright/test';
+import {TodoListPage} from "./pages/TodoListPage";
+let todoListPage: TodoListPage;
+
+test.beforeEach(async ({page}  ) => {
+    await page.goto('');
+    todoListPage = new TodoListPage(page);
+})
+
+test('Create one TODO item', async ({ page }) => {
+    const taskName = 'Buy milk';
+    await todoListPage.addItem(taskName);
+    const buyMilkTask = todoListPage.taskItem.filter({hasText: taskName});
+    await expect(buyMilkTask).toBeVisible();
+});
+
+
+test('Create 2 items and remove one of them', async ({ page }) => {
+    const task1 = 'Buy bread';
+    await todoListPage.addItem(task1);
+    const task2 = 'Buy milk';
+    await todoListPage.addItem(task2);
+    await todoListPage.removeTask(task1);
+    await expect(todoListPage.taskItem).toHaveCount(1);
+})
+
+test('Create 2 items and complete one of them', async ({ page }) => {
+    const task1 = 'Buy bread';
+    await todoListPage.addItem(task1);
+    const task2 = 'Buy milk';
+    await todoListPage.addItem(task2);
+    await todoListPage.completeTask(1);
+    await todoListPage.activeItemsFilter.click()
+    await expect(todoListPage.taskItem).toHaveCount(1);
+    await todoListPage.completedItemsFilter.click()
+    await expect(todoListPage.taskItem).toHaveCount(1);
+})
+
+test('Complete task and clear completed', async ({ page }) => {
+    const task1 = 'Buy bread';
+    await todoListPage.addItem(task1);
+    const task2 = 'Buy milk';
+    await todoListPage.addItem(task2);
+    const task3 = 'Buy water';
+    await todoListPage.addItem(task3);
+    await todoListPage.completeTask(2);
+    await todoListPage.completeTask(1);
+    await expect(todoListPage.taskItem).toHaveCount(3);
+    await todoListPage.clearCompleted.click()
+    await expect(todoListPage.taskItem).toHaveCount(1);
+})
